0658a80523448426d4b849dc3a9de3aa1c06fa47
hiroshiuk1991 <hiroshis91@icloud.com>
2020-08-25 21:04:27 +0100

    initial
    initial creation, with bare layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import axios from 'axios';
+
+import NavBar from './components/NavBar'
+import Articles from './components/Articles'
+
+export default class App extends React.Component {
+  state = {
+    articles: []
+  }
+
+
+  // componentDidMount() {
+    
+  // }
+
+  render () {
+    return (
+      <div>
+        <NavBar />
+        <Articles />
+        
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Articles.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// import Article from './components/Article'
+
+export default class Articles extends React.Component {
+    
+
+
+    render() {
+        return (
+           <h1>x</h1>
+        )
+    }
+}
